947384e0c003507e6f2859de480f69632c591c08
Vikram Pande <vikrampande783@gmail.com>
2020-09-08 17:51:58 +0530

    added department.py
    defined 4 methods.

--- python/class_intro/clg_model/department.py
+++ python/class_intro/clg_model/department.py
@@ -5,6 +5,18 @@ class Department:
         ###3 lists
         ###assign_new_hod method
         self.dept_name = dept_name
+        self.courses_list = []
+        self.professors_list = []
+        self.students_list = []
+
+        def assign_professor(self,professor):
+            pass
+        def admit_student(self,student):
+            pass
+        def assign_course(self,course):
+            pass
+        def offer_course():
+            pass
 
     def __repr__(self):
         return 'Department = %s'  % (self.dept_name)
