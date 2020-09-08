e8c75a111c1335504d8a73876a4ac6fdc04514e5
Vikram Pande <vikrampande783@gmail.com>
2020-09-08 17:13:23 +0530

    added department.py
    Removed a method.

--- python/class_intro/clg_model/department.py
+++ python/class_intro/clg_model/department.py
@@ -6,17 +6,5 @@ class Department:
         ###assign_new_hod method
         self.dept_name = dept_name
 
-    """
-    def assign_course_to_professor(self,professor,course):
-        prof_list = []
-        course_list = []
-        if professor.prof_name not in prof_list:
-            prof_list.append(professor.prof_name) 
-        for each in professor.subjects_taught:
-            if each not in course_list:
-                course_list.append(professor.prof_name)
-        print (course_list)
-    """
-
     def __repr__(self):
         return 'Department = %s'  % (self.dept_name)
